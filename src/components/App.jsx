666fd847109b92ce4b7be3caf702fff68e21f819
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-12-04 22:52:15 +0200

    04,12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,6 +34,9 @@ const App = () => {
           toast.error('Nothing found for your request', { autoClose: 3000 });
           return;
         }
+        if (page === 1) {
+          positiveResponse();
+        }
 
         const imagesData = response.map(
           ({ id, tags, webformatURL, largeImageURL }) => {
@@ -88,7 +91,6 @@ const App = () => {
     <AppContainer>
       <Searchbar onSubmit={addSearchQuery} />
       <ImageGallery images={images} />
-      {page === 1 && images.length > 0 && positiveResponse()}
       {images.length > 0 && countTotalPages(totalResults) !== page && (
         <LoadMoreBtn onClick={pageIncrement} />
       )}
@@ -100,103 +102,3 @@ const App = () => {
 };
 
 export default App;
-
-// class App extends Component {
-//   state = {
-//     error: false,
-//     page: 1,
-//     totalResults: null,
-//     searchQuery: '',
-//     images: [],
-//     isLoading: false,
-//   };
-
-//! async componentDidUpdate(_, prevState) {
-//   const { page, searchQuery } = this.state;
-//   if (page !== prevState.page || searchQuery !== prevState.searchQuery) {
-//     try {
-//       this.setState({ isLoading: true });
-//       const responseWithTotal = await findImages(searchQuery, page);
-//       const totalResults = responseWithTotal.totalHits;
-
-//       const response = responseWithTotal.hits;
-
-//       if (response.length === 0) {
-//         toast.error('Nothing found for your request', { autoClose: 3000 });
-//         return;
-//       }
-
-//       if (this.state.page === 1) {
-//         this.positiveResponse();
-//       }
-
-//       const imagesData = response.map(
-//         ({ id, tags, webformatURL, largeImageURL }) => {
-//           return {
-//             id,
-//             tags,
-//             webformatURL,
-//             largeImageURL,
-//           };
-//         }
-//       );
-
-//       this.setState(prevState => {
-//         return {
-//           images: [...prevState.images, ...imagesData],
-//           totalResults,
-//         };
-//       });
-//     } catch (error) {
-//       this.setState({ error: true });
-//       console.log(error);
-//     } finally {
-//       this.setState({ isLoading: false });
-//     }
-//   }
-// }
-
-//!   setSearchQuery = query => {
-//     if (!query) {
-//       toast.warn('Sorry, search field if empty :(', { autoClose: 3000 });
-//       return;
-//     }
-
-//     this.setState({ images: [], page: 1, searchQuery: query });
-//   };
-
-//! pageIncrement = () => {
-//   this.setState(prevState => {
-//     return { page: prevState.page + 1 };
-//   });
-// };
-
-//! countTotalPages = () => {
-//   const totalPages = Math.ceil(this.state.totalResults / 12);
-//   return totalPages;
-// };
-
-// positiveResponse = () => {
-//   toast.success(`Hooray! We found ${this.state.totalResults} images.`, {
-//     autoClose: 3000,
-//   });
-// };
-
-//   render() {
-//     const { page, images, totalResults, isLoading } = this.state;
-//     const totalPages = this.countTotalPages();
-//     return (
-//       <AppContainer>
-//         <Searchbar onSubmit={this.setSearchQuery} />
-//         <ImageGallery images={images} />
-//         {images.length > 0 && totalPages !== page && (
-//           <LoadMoreBtn onClick={this.pageIncrement} />
-//         )}
-//         {isLoading && <Loader />}
-//         <ToastContainer />
-//         {page === this.countTotalPages(totalResults) && <Finish />}
-//       </AppContainer>
-//     );
-//   }
-// }
-// export default App;
